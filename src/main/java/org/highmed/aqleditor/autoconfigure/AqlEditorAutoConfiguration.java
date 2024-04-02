bea0072ec63f1bcad9a62b03b55ecd004953580e
Marcus Fabarius <marcus.fabarius@sva.de>
2024-04-02 14:12:01 +0200

    fix config

--- src/main/java/org/highmed/aqleditor/autoconfigure/AqlEditorAutoConfiguration.java
+++ src/main/java/org/highmed/aqleditor/autoconfigure/AqlEditorAutoConfiguration.java
@@ -23,7 +23,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("org.ehrbase.aqleditor")
+@ComponentScan("org.highmed.aqleditor")
 public class AqlEditorAutoConfiguration {
 
 }
